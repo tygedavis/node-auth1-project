b1dfb56d1777aac4c7224f0d0a2392063ea68557
tygedavis <tyge.davis@gmail.com>
2020-01-27 13:41:51 -0700

    Migrations completed

--- database/migrations/20200127133009_users.js
+++ database/migrations/20200127133009_users.js
@@ -1,8 +1,17 @@
 
 exports.up = function(knex) {
+  user.increments();
+
+  users
+    .string('username')
+    .notNullable()
+    .unique();
   
+  users
+    .string('password', 128)
+    .notNullable();
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists('users';)
 };
