3836446eb2507b1722d65b6fb6d7a7fa580530bf
tygedavis <tyge.davis@gmail.com>
2020-01-28 13:41:36 -0700

    Sessions are stored on the database

--- api/configure-middleware.js
+++ api/configure-middleware.js
@@ -2,17 +2,27 @@ const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
 const session = require('express-session');
+const KnexSessionStore = require('connect-session-knex')(session);
+
+const dbConnection = require('../database/dbConfig.js');
 
 const sessionConfig = {
   name: 'CookieMonster', //it's a good idea to keep the name different
   secret: process.env.SESSION_SECRET || 'The Cookie Monster likes to eat cookies',
   cookie: {
-    maxAge: 1000 * 30,
+    maxAge: 1000 * 60,
     secure: false, //REMEBER to change this to true after production
     httpOnly: true,
   },
   resave: false,
-  saveUninitialized: false //If this is true you will be arrested... just keep it as false.
+  saveUninitialized: false, //If this is true you will be arrested... just keep it as false.
+  store: new KnexSessionStore({
+    knex: dbConnection,
+    tablename: 'sessions',
+    sidfieldname: "sid",
+    createtable: true,
+    clearInterval: 60000
+  })
 }
 
 module.exports = function(server) {
